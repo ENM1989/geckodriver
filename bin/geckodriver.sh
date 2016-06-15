6d87fb8791535d67c0d6c82d2a955373ab2e0c94
Nico Müller <user@example.com>
2016-06-15 20:41:22 +0200

    edited geckodriver.sh

--- bin/geckodriver.sh
+++ bin/geckodriver.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-exec geckodriver
+SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH=$SELF_PATH/$(basename -- "$0")
+while [ -h "$SELF_PATH" ]; do
+    DIR=$(dirname -- "$SELF_PATH")
+    SYM=$(readlink "$SELF_PATH")
+    SELF_PATH=$(cd "$DIR" && cd $(dirname -- "$SYM") && pwd)/$(basename -- "$SYM")
+done
+
+echo "${SELF_PATH}"
+
+exec "${SELF_PATH}" "$@"
